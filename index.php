f33b3e260e2e64c3dd6226d255b2126574d2076b
fahrial467 <fahrialababil@gmail.com>
2025-05-30 21:35:32 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba GIT Remote 2</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor consectetur vero laborum, eius et provident! Porro hic magni enim minima, in suscipit aut at! Consequuntur accusamus doloremque obcaecati ratione dignissimos!</p>
+    
+</body>
+</html>
